ac0f67cc6e9096c41bc45190cce1fef073b58d75
Declan <dwright1991@gmail.com>
2020-04-25 15:54:11 +0100

    initial project creation

---
+++ xam-quizapp/xam-quizapp/xam-quizapp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using xam_quizapp.Models;
+
+namespace xam_quizapp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
