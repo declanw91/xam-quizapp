a85b3a2c9a70b8b554a497af597ea3e59bf9f448
Declan <dwright1991@gmail.com>
2020-04-26 15:26:11 +0100

    Added question controller

---
+++ xam-quizapp/xam-quizapp/xam-quizapp/Controllers/QuestionController.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json.Linq;
+using quizapp.Models;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace quizapp.Controllers
+{
+    public class QuestionController
+    {
+        private HttpClient _requestClient;
+        public QuestionController()
+        {
+            _requestClient = new HttpClient();
+        }
+        public async Task<List<QuizQuestion>> GetQuizQuestions(string category, string difficulty)
+        {
+            var questionList = new List<QuizQuestion>();
+            var questionJson = await GetQuestionJson(category, difficulty);
+            if (questionJson != null)
+            {
+                var questions = questionJson.GetValue("results") as JArray;
+                foreach (var item in questions)
+                {
+                    questionList.Add(item.ToObject<QuizQuestion>());
+                }
+            }
+            return questionList;
+        }
+
+        private string BuildRequestUrl(string category, string difficulty)
+        {
+            var questionUrl = $"https://opentdb.com/api.php?amount=10&type=multiple&category={category}&difficulty={difficulty.ToLowerInvariant()}";
+            return questionUrl;
+        }
+
+        private async Task<JObject> GetQuestionJson(string category, string difficulty)
+        {
+            JObject response = null;
+            var questionUrl = BuildRequestUrl(category, difficulty);
+            using (_requestClient)
+            {
+                var json = await _requestClient.GetStringAsync(questionUrl);
+                response = JObject.Parse(json);
+            }
+            return response;
+        }
+    }
+}
---
+++ xam-quizapp/xam-quizapp/xam-quizapp/Models/QuizQuestion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace quizapp.Models
+{
+    public class QuizQuestion
+    {
+        public string Category { get; set; }
+        public string Type { get; set; }
+        public string Difficulty { get; set; }
+        public string Question { get; set; }
+        public string Correct_Answer { get; set; }
+        public Array Incorrect_Answers { get; set; }
+    }
+}
