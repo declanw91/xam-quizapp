107a08a8066cbd58c4f9a131b3d4d2e4c919a16c
Declan <dwright1991@gmail.com>
2020-05-08 16:27:57 +0100

    Dashboard image updates

--- xam-quizapp/xam-quizapp/xam-quizapp.Android/Resources/Resource.designer.cs
+++ xam-quizapp/xam-quizapp/xam-quizapp.Android/Resources/Resource.designer.cs
@@ -8445,31 +8445,37 @@ namespace quizapp.Droid
 			public const int quiz = 2131165308;
 			
 			// aapt resource value: 0x7F07007D
-			public const int randomquestions = 2131165309;
+			public const int quizbackground = 2131165309;
 			
 			// aapt resource value: 0x7F07007E
-			public const int roundedbg = 2131165310;
+			public const int quizbackgroundv2 = 2131165310;
 			
 			// aapt resource value: 0x7F07007F
-			public const int roundedbgdark = 2131165311;
+			public const int randomquestions = 2131165311;
 			
 			// aapt resource value: 0x7F070080
-			public const int settings = 2131165312;
+			public const int roundedbg = 2131165312;
 			
 			// aapt resource value: 0x7F070081
-			public const int tooltip_frame_dark = 2131165313;
+			public const int roundedbgdark = 2131165313;
 			
 			// aapt resource value: 0x7F070082
-			public const int tooltip_frame_light = 2131165314;
+			public const int settings = 2131165314;
 			
 			// aapt resource value: 0x7F070083
-			public const int trophy = 2131165315;
+			public const int tooltip_frame_dark = 2131165315;
 			
 			// aapt resource value: 0x7F070084
-			public const int vicon = 2131165316;
+			public const int tooltip_frame_light = 2131165316;
 			
 			// aapt resource value: 0x7F070085
-			public const int xamarin_logo = 2131165317;
+			public const int trophy = 2131165317;
+			
+			// aapt resource value: 0x7F070086
+			public const int vicon = 2131165318;
+			
+			// aapt resource value: 0x7F070087
+			public const int xamarin_logo = 2131165319;
 			
 			static Drawable()
 			{
