e1dfd66a1a8926510d18d076b725a0cd4bf3b1b4
Declan <dwright1991@gmail.com>
2020-05-08 14:22:41 +0100

    icons updates

--- xam-quizapp/xam-quizapp/xam-quizapp.Android/Resources/Resource.designer.cs
+++ xam-quizapp/xam-quizapp/xam-quizapp.Android/Resources/Resource.designer.cs
@@ -8442,25 +8442,28 @@ namespace quizapp.Droid
 			public const int quiz = 2131165307;
 			
 			// aapt resource value: 0x7F07007C
-			public const int roundedbg = 2131165308;
+			public const int randomquestions = 2131165308;
 			
 			// aapt resource value: 0x7F07007D
-			public const int roundedbgdark = 2131165309;
+			public const int roundedbg = 2131165309;
 			
 			// aapt resource value: 0x7F07007E
-			public const int settings = 2131165310;
+			public const int roundedbgdark = 2131165310;
 			
 			// aapt resource value: 0x7F07007F
-			public const int tooltip_frame_dark = 2131165311;
+			public const int settings = 2131165311;
 			
 			// aapt resource value: 0x7F070080
-			public const int tooltip_frame_light = 2131165312;
+			public const int tooltip_frame_dark = 2131165312;
 			
 			// aapt resource value: 0x7F070081
-			public const int trophy = 2131165313;
+			public const int tooltip_frame_light = 2131165313;
 			
 			// aapt resource value: 0x7F070082
-			public const int xamarin_logo = 2131165314;
+			public const int trophy = 2131165314;
+			
+			// aapt resource value: 0x7F070083
+			public const int xamarin_logo = 2131165315;
 			
 			static Drawable()
 			{
